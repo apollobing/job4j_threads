f803b497cbd295173ffd15e0e2dcd636b3c1d761
apollobing <taras.career@gmail.com>
2024-01-15 13:01:13 +0200

    fix: change main method - 3. Состояние нити. [#229175]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -3,17 +3,16 @@ package ru.job4j.concurrent;
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(
-                () -> { }
+                () -> System.out.println(Thread.currentThread().getName())
         );
         Thread second = new Thread(
-                () -> { }
+                () -> System.out.println(Thread.currentThread().getName())
         );
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED
-                && second.getState() != Thread.State.TERMINATED) {
-            System.out.println(first.getName());
-            System.out.println(second.getName());
+                || second.getState() != Thread.State.TERMINATED) {
+            System.out.println("main is waiting to stop first and second threads.");
         }
         System.out.println("Done!");
     }
